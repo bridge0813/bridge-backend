3b5b9f3485988db33f7f11c3bb1dab7e02b4f488
lalabulla <dlrbgus722@naver.com>
2023-09-04 01:35:49 +0900

    REFACTOR : @Date 어노테이션 삭제

--- src/main/java/com/Bridge/bridge/security/apple/ApplePublicKeys.java
+++ src/main/java/com/Bridge/bridge/security/apple/ApplePublicKeys.java
@@ -2,7 +2,6 @@ package com.Bridge.bridge.security.apple;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
