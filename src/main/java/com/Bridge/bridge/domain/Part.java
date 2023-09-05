95185ea6a2eb08d3533410c57bdba82b9cac548a
dlwlals1289 <dlwlals1289@gmail.com>
2023-09-05 18:18:22 +0900

    FIX : Part - 모집 분야 속성 추가 & 모집 기술 스택 자료형 List로 변환

--- src/main/java/com/Bridge/bridge/domain/Part.java
+++ src/main/java/com/Bridge/bridge/domain/Part.java
@@ -3,6 +3,7 @@ package com.Bridge.bridge.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
+import java.util.List;
 import java.util.Objects;
 
 @Embeddable
@@ -10,13 +11,16 @@ import java.util.Objects;
 @NoArgsConstructor
 public class Part {
 
+    private String recruitPart;     // 모집 파트
+
     private int recruitNum;         //모집인원 수
 
-    private String recruitSkill;    //모집 분야
+    private List<String> recruitSkill;    //모집 기술 스택
 
     private String requirement; // 모집 요건
 
-    public Part(int recruitNum, String recruitSkill, String requirement) {
+    public Part(String recruitPart, int recruitNum, List<String> recruitSkill, String requirement) {
+        this.recruitPart = recruitPart;
         this.recruitNum = recruitNum;
         this.recruitSkill = recruitSkill;
         this.requirement = requirement;
@@ -27,11 +31,12 @@ public class Part {
         if (this == o) return true;
         if (!(o instanceof Part)) return false;
         Part part = (Part) o;
-        return recruitNum == part.recruitNum && Objects.equals(recruitSkill, part.recruitSkill) && Objects.equals(requirement, part.requirement);
+        return recruitNum == part.recruitNum && Objects.equals(recruitPart, part.recruitPart) && Objects.equals(recruitSkill, part.recruitSkill);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(recruitNum, recruitSkill, requirement);
+        return Objects.hash(recruitPart, recruitNum, recruitSkill);
     }
+
 }
