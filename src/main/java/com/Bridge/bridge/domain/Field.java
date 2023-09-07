2263e2ef72411d797cd1dd44bf358178d5e64396
lalabulla <dlrbgus722@naver.com>
2023-09-08 01:55:44 +0900

    FIX : ID 필드 추가

--- src/main/java/com/Bridge/bridge/domain/Field.java
+++ src/main/java/com/Bridge/bridge/domain/Field.java
@@ -2,14 +2,22 @@ package com.Bridge.bridge.domain;
 
 import lombok.Getter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
 @Getter
 public class Field {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "field_id")
+    private Long id;
 
     private String fieldName; // 관심 분야
 
