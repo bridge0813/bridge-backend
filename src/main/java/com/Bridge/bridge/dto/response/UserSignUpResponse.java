c44e099db09238965e01b6ddd55640daca8e0b26
lalabulla <dlrbgus722@naver.com>
2023-09-08 17:20:54 +0900

    FEAT: 소셜 로그인 후 이름 값 요청 응답 DTO

---
+++ src/main/java/com/Bridge/bridge/dto/response/UserSignUpResponse.java
@@ -0,0 +1,11 @@
+package com.Bridge.bridge.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserSignUpResponse {
+
+    private Long userId;
+}
