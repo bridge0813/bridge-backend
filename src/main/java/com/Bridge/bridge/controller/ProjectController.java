8424e2432fac5dcf9ddbc657a6773c6837b736c4
dlwlals1289 <dlwlals1289@gmail.com>
2023-09-15 21:16:42 +0900

    FEAT : 프로젝트 모집글 수정 기능 Controller Test중

--- src/main/java/com/Bridge/bridge/controller/ProjectController.java
+++ src/main/java/com/Bridge/bridge/controller/ProjectController.java
@@ -18,30 +18,30 @@ public class ProjectController {
     private final ProjectService projectService;
 
 
-    // 프로젝트 모집글 작성
-    @PostMapping("/project")
-    public ResponseEntity createProject(@RequestBody ProjectRequestDto projectRequestDto, Long userId){
-        HttpStatus result = projectService.createProject(projectRequestDto, userId);
-        return new ResponseEntity(result);
-    }
-
-    // 검색어 기준으로 프로젝트 모집글 조회
-    @GetMapping("/project")
-    public List<ProjectListDto> searchProject(@RequestParam String searchWord){
-        return projectService.findByTitleAndContent(searchWord);
-    }
-
-    // 프로젝트 모집글 삭제
-    @DeleteMapping("/project")
-    public ResponseEntity deleteProject(@RequestParam Long projectId, @RequestBody Long userId){
-        HttpStatus result = projectService.deleteProject(projectId, userId);
-        return new ResponseEntity(result);
-    }
-
-    // 프로젝트 모집글 수정
-    @PutMapping("/project")
-    public ProjectResponseDto updateProject(@RequestParam Long projectId, @RequestBody Long userId, ProjectRequestDto projectRequestDto){
-        return projectService.updateProject(projectId, userId, projectRequestDto);
-    }
+//    // 프로젝트 모집글 작성
+//    @PostMapping("/project")
+//    public ResponseEntity createProject(@RequestBody ProjectRequestDto projectRequestDto, Long userId){
+//        HttpStatus result = projectService.createProject(projectRequestDto, userId);
+//        return new ResponseEntity(result);
+//    }
+//
+//    // 검색어 기준으로 프로젝트 모집글 조회
+//    @GetMapping("/project")
+//    public List<ProjectListDto> searchProject(@RequestParam String searchWord){
+//        return projectService.findByTitleAndContent(searchWord);
+//    }
+//
+//    // 프로젝트 모집글 삭제
+//    @DeleteMapping("/project")
+//    public ResponseEntity deleteProject(@RequestParam Long projectId, @RequestBody Long userId){
+//        HttpStatus result = projectService.deleteProject(projectId, userId);
+//        return new ResponseEntity(result);
+//    }
+//
+//    // 프로젝트 모집글 수정
+//    @PutMapping("/project")
+//    public ProjectResponseDto updateProject(@RequestParam Long projectId, @RequestBody Long userId, ProjectRequestDto projectRequestDto){
+//        return projectService.updateProject(projectId, userId, projectRequestDto);
+//    }
 
 }
--- src/test/java/com/Bridge/bridge/controller/ProjectControllerTest.java
+++ src/test/java/com/Bridge/bridge/controller/ProjectControllerTest.java
@@ -15,13 +15,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -44,96 +45,177 @@ class ProjectControllerTest {
     @Autowired
     private ProjectService projectService;
 
-    @Test
-    @DisplayName("모집글 생성")
-    void createProject() throws Exception {
-        // given
-        User user = new User("test1@gmaill.com", "apple");
-        User newUser = userRepository.save(user);
-
-        List<String> skill = new ArrayList<>();
-        skill.add("Java");
-        skill.add("Spring boot");
-
-        List<PartRequestDto> recruit = new ArrayList<>();
-        recruit.add(PartRequestDto.builder()
-                .recruitPart("backend")
-                .recruitNum(3)
-                .recruitSkill(skill)
-                .requirement("아무거나")
-                .build());
-
-        ProjectRequestDto newProject = ProjectRequestDto.builder()
-                .title("New project")
-                .overview("This is new Project.")
-                .dueDate("2023-09-07")
-                .startDate("2023-09-11")
-                .endDate("2023-09-30")
-                .recruit(recruit)
-                .tagLimit(new ArrayList<>())
-                .meetingWay("Offline")
-                .userEmail(newUser.getEmail())
-                .stage("Before Start")
-                .build();
-
-        String body = objectMapper.writeValueAsString(newProject);
-
-        // when
-        mockMvc.perform(post("/project")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(body))
-                .andExpect(status().isOk()) // 응답 status를 ok로 테스트
-                .andDo(print());
-
-    }
-
-    @Test
-    @DisplayName("모집글 삭제")
-    void deleteProject() throws Exception {
-        // given
-        User user = new User("test3@gmaill.com", "apple");
-        userRepository.save(user);
-
-        List<String> skill = new ArrayList<>();
-        skill.add("Java");
-        skill.add("Spring boot");
-
-        List<PartRequestDto> recruit = new ArrayList<>();
-        recruit.add(PartRequestDto.builder()
-                .recruitPart("backend")
-                .recruitNum(3)
-                .recruitSkill(skill)
-                .requirement("아무거나")
-                .build());
-
-        ProjectRequestDto newProject = ProjectRequestDto.builder()
-                .title("New project")
-                .overview("This is new Project.")
-                .dueDate("2023-09-07")
-                .startDate("2023-09-11")
-                .endDate("2023-09-30")
-                .recruit(recruit)
-                .tagLimit(new ArrayList<>())
-                .meetingWay("Offline")
-                .userEmail(user.getEmail())
-                .stage("Before Start")
-                .build();
-
-        projectService.createProject(newProject, user.getId());
-
-        Long userId = user.getId();
-        Long projectId = projectRepository.findByUser_Id(userId).get().getId();
-
-
-        // when
-        mockMvc.perform(delete("/project")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .param("projectId", String.valueOf(projectId))
-                        .content(objectMapper.writeValueAsString(userId)))
-                .andExpect(status().is(202)) // 응답 status를 ok로 테스트
-                .andDo(print());
-
-    }
-
+//    @Test
+//    @DisplayName("모집글 생성")
+//    void createProject() throws Exception {
+//        // given
+//        User user = new User("test1@gmaill.com", "apple");
+//        User newUser = userRepository.save(user);
+//
+//        List<String> skill = new ArrayList<>();
+//        skill.add("Java");
+//        skill.add("Spring boot");
+//
+//        List<PartRequestDto> recruit = new ArrayList<>();
+//        recruit.add(PartRequestDto.builder()
+//                .recruitPart("backend")
+//                .recruitNum(3)
+//                .recruitSkill(skill)
+//                .requirement("아무거나")
+//                .build());
+//
+//        ProjectRequestDto newProject = ProjectRequestDto.builder()
+//                .title("New project")
+//                .overview("This is new Project.")
+//                .dueDate("2023-09-07")
+//                .startDate("2023-09-11")
+//                .endDate("2023-09-30")
+//                .recruit(recruit)
+//                .tagLimit(new ArrayList<>())
+//                .meetingWay("Offline")
+//                .userEmail(newUser.getEmail())
+//                .stage("Before Start")
+//                .build();
+//
+//        String body = objectMapper.writeValueAsString(newProject);
+//
+//        // when
+//        mockMvc.perform(post("/project")
+//                    .contentType(MediaType.APPLICATION_JSON)
+//                    .content(body))
+//                .andExpect(status().isOk()) // 응답 status를 ok로 테스트
+//                .andDo(print());
+//
+//    }
+//
+//    @Test
+//    @DisplayName("모집글 삭제")
+//    void deleteProject() throws Exception {
+//        // given
+//        User user = new User("test3@gmaill.com", "apple");
+//        userRepository.save(user);
+//
+//        List<String> skill = new ArrayList<>();
+//        skill.add("Java");
+//        skill.add("Spring boot");
+//
+//        List<PartRequestDto> recruit = new ArrayList<>();
+//        recruit.add(PartRequestDto.builder()
+//                .recruitPart("backend")
+//                .recruitNum(3)
+//                .recruitSkill(skill)
+//                .requirement("아무거나")
+//                .build());
+//
+//        ProjectRequestDto newProject = ProjectRequestDto.builder()
+//                .title("New project")
+//                .overview("This is new Project.")
+//                .dueDate("2023-09-07")
+//                .startDate("2023-09-11")
+//                .endDate("2023-09-30")
+//                .recruit(recruit)
+//                .tagLimit(new ArrayList<>())
+//                .meetingWay("Offline")
+//                .userEmail(user.getEmail())
+//                .stage("Before Start")
+//                .build();
+//
+//        projectService.createProject(newProject, user.getId());
+//
+//        Long userId = user.getId();
+//        Long projectId = projectRepository.findByUser_Id(userId).get().getId();
+//
+//
+//        // when
+//        mockMvc.perform(delete("/project")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .param("projectId", String.valueOf(projectId))
+//                        .content(objectMapper.writeValueAsString(userId)))
+//                .andExpect(status().is(202)) // 응답 status를 ok로 테스트
+//                .andDo(print());
+//
+//    }
+//
+//    @Test
+//    @DisplayName("모집글 수정")
+//    void updateProject() throws Exception {
+//        // given
+//        User user = new User("updateTest@gmaill.com", "update");
+//        userRepository.save(user);
+//
+//        List<String> skill = new ArrayList<>();
+//        skill.add("Java");
+//        skill.add("Spring boot");
+//
+//        List<PartRequestDto> recruit = new ArrayList<>();
+//        recruit.add(PartRequestDto.builder()
+//                .recruitPart("backend")
+//                .recruitNum(3)
+//                .recruitSkill(skill)
+//                .requirement("아무거나")
+//                .build());
+//
+//        ProjectRequestDto newProject = ProjectRequestDto.builder()
+//                .title("New project")
+//                .overview("This is new Project.")
+//                .dueDate("2023-09-07")
+//                .startDate("2023-09-11")
+//                .endDate("2023-09-30")
+//                .recruit(recruit)
+//                .tagLimit(new ArrayList<>())
+//                .meetingWay("Offline")
+//                .userEmail(user.getEmail())
+//                .stage("Before Start")
+//                .build();
+//
+//        projectService.createProject(newProject, user.getId());
+//
+//
+//
+//
+//        List<String> updateSkill = new ArrayList<>();
+//        updateSkill.add("Javascript");
+//        updateSkill.add("React");
+//
+//        List<PartRequestDto> updateRecruit = new ArrayList<>();
+//        updateRecruit.add(PartRequestDto.builder()
+//                .recruitPart("frontend")
+//                .recruitNum(2)
+//                .recruitSkill(updateSkill)
+//                .requirement("화이팅")
+//                .build());
+//
+//        ProjectRequestDto updateProject = ProjectRequestDto.builder()
+//                .title("Update project")
+//                .overview("This is Updated Project.")
+//                .dueDate("2023-09-07")
+//                .startDate("2023-09-11")
+//                .endDate("2023-09-30")
+//                .recruit(updateRecruit)
+//                .tagLimit(new ArrayList<>())
+//                .meetingWay("Offline")
+//                .userEmail(user.getEmail())
+//                .stage("Before Start")
+//                .build();
+//
+//
+//
+//        Long userId = user.getId();
+//        Long projectId = projectRepository.findByUser_Id(userId).get().getId();
+//
+//
+//        // when
+//        MultiValueMap<String, String> data = new LinkedMultiValueMap<>();
+//        data.add("userId", userId.toString());
+//        data.add("ProjectRequestDto", updateProject.toString());
+//
+//        mockMvc.perform(put("/project")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .param("projectId", String.valueOf(projectId))
+//                        .content(objectMapper.writeValueAsString(data)))
+//                .andExpect(status().is(202)) // 응답 status를 ok로 테스트
+//                .andDo(print());
+//
+//    }
 
 }
