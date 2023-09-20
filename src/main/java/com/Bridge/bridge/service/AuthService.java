3fb129d4fc7e892e46a5f82222d9d29eab434c36
lalabulla <dlrbgus722@naver.com>
2023-09-20 23:56:38 +0900

    REFACTOR : OAuth 로그인 로직 변경

--- src/main/java/com/Bridge/bridge/service/AuthService.java
+++ src/main/java/com/Bridge/bridge/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.Bridge.bridge.service;
 
+import com.Bridge.bridge.domain.Platform;
 import com.Bridge.bridge.domain.User;
 import com.Bridge.bridge.dto.response.apple.AppleMemberResponse;
 import com.Bridge.bridge.dto.response.apple.AppleResponse;
@@ -11,6 +12,8 @@ import com.Bridge.bridge.security.apple.AppleUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
 
 @Service
 @RequiredArgsConstructor
@@ -25,21 +28,25 @@ public class AuthService {
     public OAuthTokenResponse appleOAuthLogin(AppleResponse response) throws Exception {
         // 1. 토큰을 통해 회원 정보 뺴기
         AppleMemberResponse appleUser = appleUtils.getAppleMember(response.getIdToken());
-        return generateOAuthTokenResponse(appleUser.getEmail(), appleUser.getSubject(), response.getCode());
+        return generateOAuthTokenResponse(appleUser.getEmail(), Platform.APPLE, appleUser.getSubject(), response.getName());
     }
 
     @Transactional
-    public OAuthTokenResponse generateOAuthTokenResponse(String email, String platformId, String code) {
-        return userRepository.findByPlatformId(platformId)
-                .map(u -> {
-                    AppleTokenResponse accessToken = appleToken.getAccessToken(code);
-                    return new OAuthTokenResponse(accessToken.getAccessToken(), email, true, platformId);
+    public OAuthTokenResponse generateOAuthTokenResponse(String email, Platform platform, String platformId, String name) {
+        return userRepository.findIdByPlatformAndPlatformId(platform, platformId)
+                .map(userId ->  {
+                    User findUser = userRepository.findById(userId)
+                            .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 유저입니다."));
+                    //String token = createToken(findUser);
+
+                    return new OAuthTokenResponse(null, email, true, platformId);
                 })
                 .orElseGet(() -> {
-                    User user = new User(email, platformId);
-                    userRepository.save(user);
-                    AppleTokenResponse accessToken = appleToken.getAccessToken(code);
-                    return new OAuthTokenResponse(accessToken.getAccessToken(), email, false, platformId);
+                    User newUser = new User(name, email, platform, platformId);
+                    userRepository.save(newUser);
+                    //String token = createToken(newUser);
+
+                    return new OAuthTokenResponse(null, email, false, platformId);
                 });
     }
 }
