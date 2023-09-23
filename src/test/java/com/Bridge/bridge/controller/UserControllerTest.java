c5f0fe9dfc73f3d54342911a9098994a8828865d
lalabulla <dlrbgus722@naver.com>
2023-09-24 02:29:33 +0900

    REFACROR : 이름 받는 테스트 삭제 및 로그인 로직 변경에 따른 테스트 수정

--- src/test/java/com/Bridge/bridge/controller/UserControllerTest.java
+++ src/test/java/com/Bridge/bridge/controller/UserControllerTest.java
@@ -1,10 +1,10 @@
 package com.Bridge.bridge.controller;
 
+import com.Bridge.bridge.domain.Platform;
 import com.Bridge.bridge.domain.User;
 import com.Bridge.bridge.dto.request.UserFieldRequest;
 import com.Bridge.bridge.dto.request.UserProfileRequest;
 import com.Bridge.bridge.dto.request.UserRegisterRequest;
-import com.Bridge.bridge.dto.request.UserSignUpRequest;
 import com.Bridge.bridge.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
@@ -20,7 +20,6 @@ import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -37,27 +36,10 @@ class UserControllerTest {
     private UserRepository userRepository;
 
     @Test
-    @DisplayName("회원 가입시 유저 이름 등록")
-    void registerName() throws Exception {
-        //given
-        User newUser = new User("bridge@apple.com", "3d");
-        userRepository.save(newUser);
-
-        UserSignUpRequest request = new UserSignUpRequest("3d", "브릿지");
-
-        mockMvc.perform(post("/signup")
-                        .content(objectMapper.writeValueAsString(request))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.userId").value(1L))
-                .andDo(print());
-    }
-
-    @Test
     @DisplayName("회원 가입시 관심 분야 및 프로필 등록")
     void registerField() throws Exception {
         //given
-        User newUser = new User("bridge@apple.com", "3d");
+        User newUser = new User("bridge","bridge@apple.com", Platform.APPLE,"3d");
         User saveUser = userRepository.save(newUser);
 
         List<String> stack = new ArrayList<>();
