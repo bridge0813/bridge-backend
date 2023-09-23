813e8a4c2a99b351f5ec31e0cbd8420d03cec7b3
lalabulla <dlrbgus722@naver.com>
2023-09-24 02:25:12 +0900

    FEAT : 리프레쉬 토큰 필드 추가

--- src/main/java/com/Bridge/bridge/dto/response/OAuthTokenResponse.java
+++ src/main/java/com/Bridge/bridge/dto/response/OAuthTokenResponse.java
@@ -9,15 +9,18 @@ public class OAuthTokenResponse {
 
     private String accessToken;
 
+    private String refreshToken;
+
     private String email;
 
     private boolean isRegistered;
 
     private String platformId;
 
-    public OAuthTokenResponse(String accessToken, String email, boolean isRegistered, String platformId) {
+    public OAuthTokenResponse(String accessToken, String refreshToken, String email, boolean isRegistered, String platformId) {
         this.grantType = "Bearer";
         this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
         this.email = email;
         this.isRegistered = isRegistered;
         this.platformId = platformId;
