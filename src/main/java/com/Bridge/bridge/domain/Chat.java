afacc52f52ca6028ce655371aa87b8eac7ddb2fd
lalabulla <dlrbgus722@naver.com>
2023-10-06 14:21:17 +0900

    FEAT : Chat 도메인 필드 추가

--- src/main/java/com/Bridge/bridge/domain/Chat.java
+++ src/main/java/com/Bridge/bridge/domain/Chat.java
@@ -1,10 +1,10 @@
 package com.Bridge.bridge.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,8 +18,9 @@ public class Chat {
     @Column(name = "chat_id")
     private Long id;
 
-    @OneToMany(mappedBy = "chat")
-    private List<JoinChat> joinChats = new ArrayList<>();
+    private String chatRoomId; // 채팅방 고유 ID
+
+    private String roomName; // 채팅방 이름
 
     @OneToMany(mappedBy = "chat")
     private List<Message> messages = new ArrayList<>();     // 해당 채팅방이 담고있는 메세지 목록
