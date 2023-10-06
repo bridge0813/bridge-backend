899f7d1cac3610c1590ef6fa8494b00c0cf1226d
lalabulla <dlrbgus722@naver.com>
2023-10-06 14:22:07 +0900

    REFACTOR : Chat 도메인 연관관계 수정

--- src/main/java/com/Bridge/bridge/domain/Chat.java
+++ src/main/java/com/Bridge/bridge/domain/Chat.java
@@ -22,7 +22,15 @@ public class Chat {
 
     private String roomName; // 채팅방 이름
 
-    @OneToMany(mappedBy = "chat")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(referencedColumnName = "user_id", name = "make_user_id")
+    private User makeUser;      // 채팅방 만든 사람
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(referencedColumnName = "user_id", name = "receive_user_id")
+    private User receiveUser;   // 채팅방 참가한 사람
+
+    @OneToMany(mappedBy = "chat", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Message> messages = new ArrayList<>();     // 해당 채팅방이 담고있는 메세지 목록
 
 }
