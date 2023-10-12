c45166542d782b4c03680aba90a661f8097775ac
dlwlals1289 <dlwlals1289@gmail.com>
2023-10-12 22:29:32 +0900

    FEAT : 최근 검색어 삭제 ServiceTest 완료

--- src/test/java/com/Bridge/bridge/service/ProjectServiceTest.java
+++ src/test/java/com/Bridge/bridge/service/ProjectServiceTest.java
@@ -1097,7 +1097,7 @@ class ProjectServiceTest {
     @Test
     void unscrap() {
         // given
-        User user = new User("unscrap", "unscrap@gmail.com", Platform.APPLE, "unscrapTest");
+        User user = new User("user1", "user1@gmail.com", Platform.APPLE, "Test");
         user = userRepository.save(user);
 
         List<String> skill = new ArrayList<>();
@@ -1153,7 +1153,7 @@ class ProjectServiceTest {
     @Test
     void resentSearchWord() {
         // given
-        User user = new User("scrap", "scrap@gmail.com", Platform.APPLE, "scrapTest");
+        User user = new User("user1", "user1@gmail.com", Platform.APPLE, "Test");
         user = userRepository.save(user);
 
         SearchWord newSearch1 = SearchWord.builder()
@@ -1185,6 +1185,43 @@ class ProjectServiceTest {
         Assertions.assertThat(searchWordResponseDto.get(0).getSearchWord()).isEqualTo("검색어1");
         Assertions.assertThat(searchWordResponseDto.get(1).getSearchWord()).isEqualTo("검색어2");
         Assertions.assertThat(searchWordResponseDto.get(2).getSearchWord()).isEqualTo("검색어3");
+    }
+
+    @DisplayName("최근 검색어 삭제")
+    @Test
+    void deleteSearchWord() {
+        // given
+        User user = new User("user1", "user1@gmail.com", Platform.APPLE, "Test");
+        user = userRepository.save(user);
+
+        SearchWord newSearch1 = SearchWord.builder()
+                .content("검색어1")
+                .user(user)
+                .history(LocalDateTime.now())
+                .build();
+        SearchWord newSearch2 = SearchWord.builder()
+                .content("검색어2")
+                .user(user)
+                .history(LocalDateTime.now())
+                .build();
+        SearchWord newSearch3 = SearchWord.builder()
+                .content("검색어3")
+                .user(user)
+                .history(LocalDateTime.now())
+                .build();
+
+        searchWordRepository.save(newSearch1);
+        searchWordRepository.save(newSearch2);
+        searchWordRepository.save(newSearch3);
+
+        user.getSearchWords().add(newSearch1);
+        user.getSearchWords().add(newSearch2);
+        user.getSearchWords().add(newSearch3);
+
+        // when
+        List<SearchWordResponseDto> searchWordResponseDto = projectService.deleteSearchWord(user.getId(), newSearch1.getId());
+        Assertions.assertThat(searchWordResponseDto.get(0).getSearchWord()).isEqualTo("검색어2");
+        Assertions.assertThat(searchWordResponseDto.get(1).getSearchWord()).isEqualTo("검색어3");
     }
 
 
