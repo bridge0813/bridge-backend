2c524fa9bab3d5a2944c5c49b169cc566e9ae7a9
lalabulla <dlrbgus722@naver.com>
2023-10-13 15:42:06 +0900

    FIX : 어노테이션 수정

--- src/main/java/com/Bridge/bridge/service/ProjectService.java
+++ src/main/java/com/Bridge/bridge/service/ProjectService.java
@@ -20,13 +20,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 
 @Service
-@Transactional(readOnly = false)
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ProjectService {
 
