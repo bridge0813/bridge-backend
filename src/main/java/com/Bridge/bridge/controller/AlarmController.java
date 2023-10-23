d33b5339c422bd003f03b164ae1f8ddbf55c9c38
dlwlals1289 <dlwlals1289@gmail.com>
2023-10-23 22:03:14 +0900

    FEAT : 모든 알람 삭제 기능 Controller 구현

--- src/main/java/com/Bridge/bridge/controller/AlarmController.java
+++ src/main/java/com/Bridge/bridge/controller/AlarmController.java
@@ -3,6 +3,8 @@ package com.Bridge.bridge.controller;
 import com.Bridge.bridge.dto.response.AllAlarmResponse;
 import com.Bridge.bridge.service.AlarmService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +26,11 @@ public class AlarmController {
     @PostMapping("/alarms")
     public List<AllAlarmResponse> getAllOfAlarms(@RequestBody Long userId){
         return alarmService.getAllOfAlarms(userId);
+    }
+
+    @DeleteMapping("/alarms")
+    public ResponseEntity<?> deleteAllOfAlarms(@RequestBody Long userId){
+        boolean result = alarmService.deleteAllAlarms(userId);
+        return ResponseEntity.ok(result);
     }
 }
