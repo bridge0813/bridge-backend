f1a3a02432c6891ef2fb6a5a70df08b79975703f
lalabulla <dlrbgus722@naver.com>
2023-10-29 02:04:19 +0900

    REFACTOR : 스택 -> ENUM으로 변경

---
+++ src/main/java/com/Bridge/bridge/domain/Stack.java
@@ -0,0 +1,24 @@
+package com.Bridge.bridge.domain;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Stack {
+
+    SWIFT("Swift"), OBJECTIVE_C("Objective-c"), UIKIT("UIKit"), SWIFTUI("SwiftUI"), RXSWIFT("RxSwift"),
+    COMBINE("Combine"), XCTEST("XCTest"), TUIST("Tuist"), REACTIVENATIVE("ReactiveNative"), FLUTTER("Flutter"),
+    KOTIIN("Kotlin"), JAVA("Java"), COMPOSE("Compose"), RXJAVA("RXJava"), COROUTINE("Coroutine"),
+    JAVASCRIPT("JavaScript"), TYPESCRIPT("TypeScript"), HTML("HTML"), CSS("CSS"), REACT("React"),
+    VUE("Vue"), ANGULAR("Angular"), SVELTE("Svelte"), JQUERY("JQuery"), BACKBONE("Backbone"), PINIA("Pinia"),
+    PYTHON("python"), C("C"), CPP("C++"), SPRING("Spring"), SPRINGBOOT("SpringBoot"),
+    NODEJS("NodeJS"), DJANGO("Django"), HIBERNATE("Hibernate"), WEBRTC("WebRTC"), MONGODB("MongoDB"),
+    MYSQL("MySQL"), POSTGRESQL("PostgreSQL"), REDIS("Redis"), MARIADB("MariaDB"), PHOTOSHOP("Photoshop"),
+    ILLUSTRATOR("Illustrator"), INDESIGN("Indesign"), ADOBEXD("AdobeXD"), FIGMA("Figma"), SKETCH("Sketch"),
+    ADOBEFLASH("AdobeFlash"), NOTION("Notion"), JIRA("Jira"), SLACK("Slack");
+
+    private final String value;
+
+}
