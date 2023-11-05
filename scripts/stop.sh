4b6d4ae65ee4c25821cf6165a39d2537c9448400
kyu-hyun <102718303+lalabulla@users.noreply.github.com>
2023-11-05 22:09:47 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/bridge.jar"
+JAR_FILE="$PROJECT_ROOT/bridge-0.0.1-SNAPSHOT.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
@@ -15,5 +15,5 @@ if [ -z $CURRENT_PID ]; then
   echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
 else
   echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
-  kill -15 $CURRENT_PID
+  kill -9 $CURRENT_PID
 fi
