09c581e59186a3f55ec3270f0c5e033b596e5a2a
kyu-hyun <102718303+lalabulla@users.noreply.github.com>
2023-11-17 23:00:59 +0900

    Update WebSocketConfig.java

--- src/main/java/com/Bridge/bridge/config/WebSocketConfig.java
+++ src/main/java/com/Bridge/bridge/config/WebSocketConfig.java
@@ -27,10 +27,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     /* 한 클라이언트에서 다른 클라이언트로 메시지를 라우팅하는데 사용될 메시지 브로커 */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // 메시지(채팅방)를 구독하는 요청 url => 즉 메시지 받을 때
+        // 메시지(채팅방)를 구독하는 요청 url -> 즉 메시지 받을 때
         registry.enableSimpleBroker("/sub");
 
-        // 메시지를 발행하는 요청 url => 즉 메시지 보낼 때
+        // 메시지를 발행하는 요청 url -> 즉 메시지 보낼 때
         registry.setApplicationDestinationPrefixes("/pub");
     }
 
