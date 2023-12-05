fed4609d1439be0db7dfe955f7115809b331f7b5
lalabulla <dlrbgus722@naver.com>
2023-12-05 23:34:53 +0900

    REFACTOR : Bootstrap 주소 Constant 로 변경

--- src/main/java/com/Bridge/bridge/util/Constant.java
+++ src/main/java/com/Bridge/bridge/util/Constant.java
@@ -2,6 +2,6 @@ package com.Bridge.bridge.util;
 
 public abstract class Constant {
     public static final String KAFKA_TOPIC = "chat";
-
     public static final String GROUP_ID = "group1";
+    public static final String BOOTSTRAP_SERVER = "54.180.195.17:9092";
 }
