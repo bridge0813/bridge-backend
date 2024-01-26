9dd602e63011a18fb6bc71ad14cf1080b0211681
kyu-hyun <102718303+lalabulla@users.noreply.github.com>
2024-01-26 22:12:25 +0900

    Update Constant.java

--- src/main/java/com/Bridge/bridge/util/Constant.java
+++ src/main/java/com/Bridge/bridge/util/Constant.java
@@ -1,8 +1,7 @@
 package com.Bridge.bridge.util;
 
 public abstract class Constant {
-    //public static final String KAFKA_TOPIC = "chat";
-    public static final String KAFKA_TOPIC = "test";
+    public static final String KAFKA_TOPIC = "chat";
     public static final String GROUP_ID = "group1";
-    public static final String BOOTSTRAP_SERVER = "localhost:9092";
+    public static final String BOOTSTRAP_SERVER = "54.180.195.17:9092";
 }
