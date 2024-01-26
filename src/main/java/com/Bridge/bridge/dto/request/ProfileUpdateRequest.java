ecac96c3c596754fff1a8a5f5b33aae0f3685a33
lalabulla <dlrbgus722@naver.com>
2024-01-26 18:39:06 +0900

    REFACTOR : 프로필 유저 이름 수정 추가

--- src/main/java/com/Bridge/bridge/dto/request/ProfileUpdateRequest.java
+++ src/main/java/com/Bridge/bridge/dto/request/ProfileUpdateRequest.java
@@ -11,18 +11,16 @@ import java.util.Map;
 @NoArgsConstructor
 public class ProfileUpdateRequest {
 
+    private String name;
     private String selfIntro;
-
     private String career;
-
-    //private List<String> stack;
     private List<FieldAndStackRequest> fieldAndStacks;
     private List<String> refLinks;
-
     private List<Long> fileIds;
 
     @Builder
-    public ProfileUpdateRequest(String selfIntro, String career, List<FieldAndStackRequest> fieldAndStacks, List<String> refLinks, List<Long> fileIds) {
+    public ProfileUpdateRequest(String name, String selfIntro, String career, List<FieldAndStackRequest> fieldAndStacks, List<String> refLinks, List<Long> fileIds) {
+        this.name = name;
         this.selfIntro = selfIntro;
         this.career = career;
         this.fieldAndStacks = fieldAndStacks;
