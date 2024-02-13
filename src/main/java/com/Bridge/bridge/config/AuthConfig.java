bd92b3004cf23d5043437af15a8d48bcad79170c
lalabulla <dlrbgus722@naver.com>
2024-02-13 18:07:34 +0900

    FEAT : 유저 권한 검사 추가

--- src/main/java/com/Bridge/bridge/config/AuthConfig.java
+++ src/main/java/com/Bridge/bridge/config/AuthConfig.java
@@ -15,7 +15,11 @@ public class AuthConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/chat/{userId}")
+                .addPathPatterns("/users/*", "/searchWords","/project","/projects/category",
+                        "/projects", "/projects/mypart", "/project/deadline",
+                        "/project/scrap","/projects/apply","/projects/apply/cancel",
+                        "/projects/apply/users","/projects/accept","/projects/reject",
+                        "/chat/*", "/alarm", "/alarms")
                 .excludePathPatterns("/home", "/login/apple");
     }
 }
